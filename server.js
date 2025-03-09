5be7465a0fc5e3b31fdc3a67ad8898622c9cd585
Zullu7 <149877818+Zullu7@users.noreply.github.com>
2025-03-09 02:15:33 -0400

    primeiro comit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 2014;
+
+// Servir arquivos estáticos (CSS, imagens, vídeos, etc.)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rota principal para servir o arquivo HTML
+app.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+   console.log(`Servidor rodando na porta ${PORT}`);
+});
